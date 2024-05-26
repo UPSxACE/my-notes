8f90a5f22ebd15c7f0dd46ca077fe11d80a77b0d
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2024-05-26 11:35:24 +0100

    refactor: reset next.config

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["uploads-ssl.webflow.com"],
-    minimumCacheTTL: 60,
-  },
-  //   async headers() {
-  //     return [
-  //       {
-  //         source: "/:all*(svg|jpg|png|webp)",
-  //         locale: false,
-  //         headers: [
-  //           {
-  //             key: "Cache-Control",
-  //             value: "public, max-age=9999999999, must-revalidate",
-  //           },
-  //           {
-  //             key: "CDN-Cache-Control",
-  //             value: "18000",
-  //           },
-  //         ],
-  //       },
-  //     ];
-  //   },
-};
+const nextConfig = {};
 
 export default nextConfig;
